c26cea165a2ca70f265d8341f26ab0731dabd994
wanghanhao <3252913677@qq.com>
2024-07-09 23:57:52 +0800

    feat:Synchronized锁升级-偏向锁

---
+++ concurrent-01/src/main/java/com/wanghanhao/concurrent/SyncLockFlag.java
@@ -0,0 +1,24 @@
+package com.wanghanhao.concurrent;
+
+import org.openjdk.jol.info.ClassLayout;
+
+import java.util.HashMap;
+
+public class SyncLockFlag {
+    // 当我们开启了偏向锁，并且没有延迟开启的时候，新创建的对象的mark word 默认就是偏向锁状态的markword。
+    // 只不过这个时候，因为没有现成争抢，除了我们的锁标志为和是否为偏向锁标志位，其他的位数都是0
+    static MyObject myobject = new MyObject();
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("=====================未偏向线程的偏向锁============================");
+        System.out.println(ClassLayout.parseInstance(myobject).toPrintable());
+        HashMap map = new HashMap();
+        map.put(myobject,""); //隐式的调用了hashcode方法
+        synchronized (myobject) {
+            System.out.println("=====================偏向锁============================");
+            System.out.println(ClassLayout.parseInstance(myobject).toPrintable());
+        }
+    }
+    static class MyObject{
+    }
+}
+
